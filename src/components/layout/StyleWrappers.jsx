06bb1972436fd0b17d22849dd6f03e94eaf8c7fe
Sayantan Samanta <sssamanta789@gmail.com>
2020-11-08 23:53:46 +0530

    Middlesection ON

--- src/components/layout/StyleWrappers.jsx
+++ src/components/layout/StyleWrappers.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Grid = styled.section`
 display : grid;
-grid-template-columns : ${props => props.col ? props.col : 'minmax(4rem,1fr) repeat(10,minmax(max-content,8rem)) minmax(4rem,1fr)'};
+grid-template-columns : ${props => props.col ? props.col : props.col2 ? props.col2 :'minmax(4rem,1fr) repeat(10,minmax(max-content,8rem)) minmax(4rem,1fr)'};
 grid-template-rows: ${props => props.row ? props.row : '100vh repeat(3,min-content)'};
 overflow: hidden;
 `
