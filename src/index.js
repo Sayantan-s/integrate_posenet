4f0d8c50e83ee81bf4d54985117ded61a38d19cb
Sayantan Samanta <sssamanta789@gmail.com>
2020-11-19 23:27:16 +0530

    Added dynamic routes

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ import Landing from './Landing';
 import './assets/fonts/stylesheet.css' 
 import { Provider } from  'react-redux'
 import store from './store/store';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 ReactDOM.render(
- <Provider store={store}>
-    <React.StrictMode>
-      <Landing />
-    </React.StrictMode>
- </Provider>,
+<BrowserRouter>
+  <Provider store={store}>
+      <React.StrictMode>
+        <Switch>
+          <Route path="/" component={Landing} />
+        </Switch>
+      </React.StrictMode>
+  </Provider>
+</BrowserRouter>,
   document.getElementById('root')
 );
 
